c6ee7fb1de61684154a9d8cc18e309077ce46b0c
ritikaasharma <69035863+ritikaasharma@users.noreply.github.com>
2020-12-08 14:10:41 +0530

    Update talkbot2.py

--- talkbot2.py
+++ talkbot2.py
@@ -8,6 +8,15 @@
 #cur = db.cursor()                     
 
 #from tkinter import *
+def games():
+    #if ans == 'yes' or ans == 'Yes' or ans == 'YES':
+        gameip = int(input("What would you like to play?\n1. Stone Paper Scissors\n2. Tic-Tac-Toe\n"))
+        if gameip == 1:
+            from subprocess import call
+            call(["python", "stone_paper_scissors.py"])
+        else:
+            from subprocess import call
+            call(["python", "tic_tac_toe_mult.py"])
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.CRITICAL)
@@ -22,28 +31,19 @@ engine.setProperty('voice', voices[1].id)
 
 import speech_recognition as sr
 r=sr.Recognizer()
-
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.conversation import Statement
+from chatterbot.trainers import ChatterBotCorpusTrainer
 #read_only=True : used to disable chatbots ability to learn after the training
 #chatterbot.logic.MathematicalEvaluation : helps to solve maths problems
 #chatterbot.logic.BestMatch : used to choose best match from the list of responses
-my_bot = ChatBot(name = 'PyBot', read_only = True, logic_adapters = ['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch']) 
-
-small_talk = ['Hi',
-              'Hey',
-              'Hello',
-              'Hi there!',
-              'How do you do ?',
-              'How are you ?']
+my_bot = ChatBot(name = 'PyBot', read_only = True, logic_adapters = ['chatterbot.logic.MathematicalEvaluation',
+                                                                        'chatterbot.logic.BestMatch']) 
+trainer = ChatterBotCorpusTrainer(my_bot)
 
 
-math_talk = ['Pythagoras Theorem : a² + b² = c²']
-math_talk1 = ['Area of circle' ,'3.14r²']
-list_trainer = ListTrainer(my_bot)
-for item in (small_talk, math_talk, math_talk1):
-    list_trainer.train(item)
+trainer.train("chatterbot.corpus.english")
 
 """def Text_to_speech(text):
     Message = text
@@ -58,32 +58,35 @@ inmode = input("Interactive mode : Audio/Text ? ")
 #cur.execute("INSERT INTO cbdata (Name) VALUES (%s)", (name))
 #db.commit()
 #db.close()
-welmsg="Welcome to TalkBot, I am Siya!"
-namemsg="What is your name"
+
+namemsg="What is your name ?"
 
 
 if(inmode=="Text"):
+    welmsg="Welcome, I am Cia!"
     print(welmsg)
     name = input("What is your name ?\n")
     while True:
-        req = input(name + ': ')
+        request = input(name + ': ')
         #req = req.upper()
     
-        if req == 'Bye' or req == 'bye' or req == 'BYE':
-            print('TalkBot: Bye, have a great day!')
+        if 'Bye' in request or 'bye' in request or 'BYE' in request:
+            print('Cia: Bye, have a great day!')
             break
+        elif 'Facts' in request or 'facts' in request or 'fact' in request or 'Fact' in request:
+            from facts import facts_func
+            print("Cia: ",facts_func())
+        elif 'games' in request or 'Games' in request or 'game' in request: 
+            print("Cia: ",games())
         else:
-            res = my_bot.get_response(text = req)
-            print("TalkBot:",res)
-        """else:
-            ans = input(("Would you like to play a game\n"))
-            if ans == 'yes' or ans == 'Yes' or ans == 'YES':
-                import stone_paper_scissors
-                exec('stone_paper_scissors')"""
+            res = my_bot.get_response(text = request)
+            print("Cia:",res)
+        
         
 
 if(inmode=="Audio"):
-    engine.say(welmsg)
+    welmsg1 = "Welcome, I am Siya!"
+    engine.say(welmsg1)
     engine.runAndWait()
 
     engine.say(namemsg)
@@ -113,35 +116,20 @@ if(inmode=="Audio"):
                 print("Your response: "+r.recognize_google(req))
             except:
                 print("Sorry, I did not get that")
-            #req = input(name + ': ')
-            #req = req.upper()
         
-        if req2 == 'Bye' or req2 == 'bye' or req2 == 'BYE':
+        if 'Bye' in req2 or 'bye' in req2 or 'BYE' in req2:
             byemsg = 'Bye, have a great day!'
             engine.say(byemsg)
             engine.runAndWait()
-            break
+            exit()
+        elif  'Facts' in req2 or 'facts' in req2 or 'fact' in req2 or 'Fact' in req2 :
+            from facts import facts_func
+            engine.say(facts_func())
+            engine.runAndWait()
+        elif 'games' in req2 or 'Games' in req2 or 'game' in req2:
+            engine.say(games())
+            engine.runAndWait()
         else:
             res = my_bot.get_response(text = req2)
-            #print("Bot:",res)
             engine.say(res)
             engine.runAndWait()
-        """else:
-            gamemsg = "Would you like to play a game?"
-            engine.say(gamemsg)
-            engine.runAndWait()
-            with sr.Microphone() as source2:
-                #r.adjust_for_ambient_noise(source, duration=0.2)
-                ans=r.listen(source2)
-                try:
-                    # using google speech recognition
-                    print("You spoke: "+r.recognize_google(ans))
-                except:
-                    print("Sorry, I did not get that")
-                        
-            #ans = input()
-            if ans == 'yes' or ans == 'Yes' or ans == 'YES':
-                import stone_paper_scissors
-                exec('stone_paper_scissors')
-        """
-        
