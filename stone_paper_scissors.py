b4bef52f50e1c3930e75840cddd4d49bac01ed8b
rohitbari2000 <60724791+rohitbari2000@users.noreply.github.com>
2021-01-03 15:31:46 +0530

    Update stone_paper_scissors.py

--- stone_paper_scissors.py
+++ stone_paper_scissors.py
@@ -12,21 +12,37 @@ db = MySQLdb.connect(
 )
 
 cursor = db.cursor()
-player = input("Enter the name of player:")
+name_msg = "Enter the name of player:"
+player = input(name_msg)
 cursor.execute("INSERT INTO scoreboard (Name) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(player,player))
+cursor.execute("INSERT INTO chathistory (Name) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(player,player))
+cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(name_msg,player))
+sps_db = """Rules for the game:1) Rock vs Scissor => Rock wins"+
+							  	"2) Rock vs Paper => Paper wins" +
+								"3) Scissor vs Paper => Scissor wins"""
 print ("\nRules for the game:\n1) Rock vs Scissor => Rock wins\n"+
 							  	"2) Rock vs Paper => Paper wins\n" +
 								"3) Scissor vs Paper => Scissor wins\n")
 
+cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(sps_db,player))
+
 user_count = 0
 comp_count = 0
 i = 0
-n = int (input ("How many rounds would you like to play?\n"))
+rounds_db = "How many rounds would you like to play?"
+n = int (input(rounds_db) + "\n")
+cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(rounds_db,player))
+cursor.execute("INSERT INTO chathistory (User) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(n,player))
 while i < n:
-	user_choice = int(input("Enter your choice: 1) Rock 2) Paper 3) Scissor\n"))
-
+	choice_db = "Enter your choice: 1) Rock 2) Paper 3) Scissor"
+	user_choice = int(input(choice_db + "\n"))
+	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(choice_db,player))
+	cursor.execute("INSERT INTO chathistory (User) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(user_choice,player))
 	while(user_choice > 3 or user_choice < 1):
-		user_choice = int(input("Enter valid choice:\n"))
+		valid_msg = "Enter valid choice:"
+		user_choice = int(input(valid_msg + "\n"))
+		cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(valid_msg,player))
+		cursor.execute("INSERT INTO chathistory (User) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(user_choice,player))
 
 	if user_choice == 1:
 		user_choice_name = 'Rock'
@@ -46,45 +62,65 @@ while i < n:
 		comp_choice_name = 'Paper'
 	else:
 		comp_choice_name = 'Scissor'
-	print ("Cia chooses..." + comp_choice_name)
+	c_choice = "Cia chooses..."
+	print (c_choice + comp_choice_name)
+	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(c_choice,player))
+	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(comp_choice_name,player))
 
 	print (user_choice_name + " v/s " + comp_choice_name)
 
 	if ((user_choice == 1 and comp_choice == 2) or
 		(user_choice == 2 and comp_choice == 1)):
-		print ("Paper wins..")
+		p_win = "Paper wins.."
+		print (p_win)
+		cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(p_win,player))
 		result = 'Paper'
 	elif ((user_choice == 1 and comp_choice == 3) or 
 		(user_choice == 3 and comp_choice == 1)):
-		print ("Rock wins..")
+		r_win = "Rock wins.."
+		print (r_win)
+		cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(r_win,player))
 		result = 'Rock'
 	else:
-		print ("Scissor wins..")
+		s_win = "scissor wins.."
+		print (s_win)
+		cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(s_win,player))
 		result = 'Scissor'
 
 	if (user_choice_name == result):
 		user_count += 1
-		print("You win this round!")
+		user_won = "You win this round!"
+		print(user_won)
+		cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(user_won,player))
 	else:
 		comp_count += 1
-		print("Cia wins this round!")
-
+		cia_won = "Cia wins this round!"
+		print(cia_won)
+		cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(cia_won,player))
 	i = i + 1
 
 if (comp_count > user_count):
-	cursor.execute("UPDATE scoreboard SET Stone_Paper_Scissors = Stone_Paper_Scissors+1 WHERE Name = 'Cia'")
-	cursor.execute("UPDATE scoreboard SET Total_wins = Total_wins+1 WHERE Name= 'Cia'")
-	print ("\nCia won the game. Better luck next time!")
+	cursor.execute("UPDATE chathistory SET Stone_Paper_Scissors = Stone_Paper_Scissors+1 WHERE Name = 'Cia'")
+	cursor.execute("UPDATE chathistory SET Total_wins = Total_wins+1 WHERE Name= 'Cia'")
+	winner_db = "Cia won the game. Better luck next time!"
+	print ("\n" + winner_db)
+	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(winner_db,player))
 elif (comp_count == user_count):
-	print ("It's a tie!")
+	tie_msg = "It's a tie!"
+	print (tie_msg)
+	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(tie_msg,player))
 else:
-	cursor.execute("UPDATE scoreboard SET Stone_Paper_Scissors = Stone_Paper_Scissors+1 WHERE Name=%s",(player,))
-	cursor.execute("UPDATE scoreboard SET Total_wins = Total_wins+1 WHERE Name=%s",(player,))
-	print ("\nHurray!!" +player+ "won!!")
+	cursor.execute("UPDATE chathistory SET Stone_Paper_Scissors = Stone_Paper_Scissors+1 WHERE Name=%s",(player,))
+	cursor.execute("UPDATE chathistory SET Total_wins = Total_wins+1 WHERE Name=%s",(player,))
+	print ("\nHurray!!" +player+ " won!!")
+	cursor.execute("INSERT INTO chathistory (Cia) VALUES ('Hurray') ON DUPLICATE KEY UPDATE Name=%s",(player,))
+	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(player,player))
+	cursor.execute("INSERT INTO chathistory (Cia) VALUES ('won') ON DUPLICATE KEY UPDATE Name=%s",(player,))
+
 print("\nScore:\nCia: " + str(comp_count) + "\n" + player + ":" + str(user_count))
-
-print ("\nThanks for playing! I hope you had fun!")
-
+final_msg = "Thanks for playing! I hope you had fun!"
+print (final_msg)
+cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(final_msg,player))
 
 db.commit()
 # print(cursor.rowcount, "record inserted")
