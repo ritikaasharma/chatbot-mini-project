94aba8ff773e958d3b04b831e5eb22a2eabef5f9
rohitbari2000 <60724791+rohitbari2000@users.noreply.github.com>
2021-01-22 16:39:22 +0530

    Update talkbot2.py

--- talkbot2.py
+++ talkbot2.py
@@ -9,16 +9,24 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.conversation import Statement
 from chatterbot.trainers import ChatterBotCorpusTrainer
-
+import pymysql
 import MySQLdb
 
+'''db = pymysql.connect(
+    "localhost",
+    "root",
+    "#root9694",
+    "chatbotdb"
+)'''
 db = MySQLdb.connect(
     host = "localhost",
     user = "root",
     passwd = "#root9694",
     database = "chatbotdb"
-)
+    )
+
 cursor = db.cursor()
+db.autocommit(True)
 name_of_talkbot = "Cia"
 cursor.execute("CREATE TABLE IF NOT EXISTS scoreboard (Name varchar(255) NOT NULL DEFAULT ' ', Stone_Paper_Scissors int DEFAULT 0, Tic_Tac_Toe_Single int DEFAULT 0, Tic_Tac_Toe_Multi int DEFAULT 0, Frequency int DEFAULT 0,Total_wins int DEFAULT 0)")
 cursor.execute("CREATE TABLE IF NOT EXISTS chathistory (Name varchar(255) DEFAULT ' ', Frequency int DEFAULT 0, User varchar(255) DEFAULT ' ', Cia varchar(500) DEFAULT ' ', Date_and_Time timestamp DEFAULT current_timestamp)")
@@ -33,20 +41,18 @@ def games():
     gameip = int(input())
     cursor.execute("INSERT INTO chathistory (User) VALUES (%s)",(gameip,))
     if gameip == 1:
-        from subprocess import call
-        call(["python", "stone_paper_scissors.py"])
+        # from subprocess import call
+        # call(["python", "sps3.py"])
+        from sps3 import spsm
+        spsm(cursor)
     elif gameip == 2:
-        from subprocess import call
-        call(["python", "tic_tac_toe_mult.py"])
+        from tic_tac_toe_mult import ttcm
+        ttcm(cursor)
 
-def ytd():
-    #if ans == 'yes' or ans == 'Yes' or ans == 'YES':
-    from subprocess import call
-    call(["python", "ytdownloader.py"])
 
 bye_msg = "Bye, have a great day!"
-gamemsg = "What would you like to play?\n1. Stone Paper Scissors\n2. Tic-Tac-Toe\n"
-gamemsg_db = "What would you like to play? 1. Stone Paper Scissors 2. Tic-Tac-Toe"
+
+
 
 
 def text():
@@ -75,29 +81,38 @@ def text():
             fact = facts_func()
             cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(fact,name))
             cursor.execute("UPDATE chathistory SET Frequency = Frequency + 1 where Name =%s",(name,))
-            print("Cia: ",fact)
-        elif 'games' in request or 'Games' in request or 'game' in request: 
+            print("Cia: "+fact)
+        elif 'games' in request or 'Games' in request or 'game' in request:
+            gamemsg = "What would you like to play?\n1. Stone Paper Scissors\n2. Tic-Tac-Toe\n"
+            gamemsg_db = "What would you like to play? 1. Stone Paper Scissors 2. Tic-Tac-Toe" 
             cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s)",(gamemsg_db,))
-            print(gamemsg)
-            print("Cia: ",games())
+            print("Cia: "+ gamemsg)
+            games()
             cursor.execute("UPDATE chathistory SET Frequency = Frequency + 1 where Name =%s",(name,))
         elif 'Language' in request or 'language' in request or 'Translator' in request or 'translator' in request or 'Translate' in request or 'translate' in request:
             print("Cia: ")
-            from subprocess import call
-            call(["python","langtranslate.py"])
+            # from subprocess import call
+            # call(["python","langtranslate.py"])
+            from langtranslate import text_translator
+            text_translator(cursor)
             cursor.execute("UPDATE chathistory SET Frequency = Frequency + 1 where Name =%s",(name,))
         elif 'YouTube' in request or 'Download' in request or 'youtube' in request or 'download a youtube video' in request:
-            print("Cia: ",ytd())
+            from ytdownloader import ytfunc
+            print("Cia: ",end="")
+            ytfunc(cursor)
             cursor.execute("UPDATE chathistory SET Frequency = Frequency + 1 where Name =%s",(name,))
         else:
-            res = my_bot.get_response(text = request)
-            print("Cia: ",res)
+            res = my_bot.get_response(request)
+            print("Cia: "+ res)
             cursor.execute("INSERT into chathistory (Cia) VALUES(%s)", [res])
-        
+    cursor.close()#newly added
+    #db.commit()
         
 
 def audio():
-    welmsg1 = "Welcome, I am Siya!"
+    welmsg1 = "Welcome, I am Siya!"      
+    #Newly Added  
+    cursor = db.cursor()
     cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s)",[welmsg1])
     engine.say(welmsg1)
     engine.runAndWait()
@@ -151,6 +166,7 @@ def audio():
             engine.say(bye_msg)
             engine.runAndWait()
             cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",[bye_msg,nm])
+            print('Cia: ' + bye_msg + nm)
             exit()
         elif  'Facts' in req2 or 'facts' in req2 or 'fact' in req2 or 'Fact' in req2 :
             from facts import facts_func
@@ -159,21 +175,28 @@ def audio():
             engine.runAndWait()
             cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(fact,nm))
             cursor.execute("UPDATE chathistory SET Frequency = Frequency + 1 where Name =%s",(nm,))
+            print("Cia: ",fact + nm)
         elif 'games' in req2 or 'Games' in req2 or 'game' in req2:
+            gamemsg_db = "What would you like to play? 1. Stone Paper Scissors 2. Tic-Tac-Toe"
+            gamemsg = "What would you like to play?\n1. Stone Paper Scissors\n2. Tic-Tac-Toe\n"
             print(gamemsg)
             engine.say(gamemsg)
+            engine.runAndWait()
             cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s)  ON DUPLICATE KEY UPDATE Name=%s",(gamemsg_db,nm))
             games()
-            engine.runAndWait()
             cursor.execute("UPDATE chathistory SET Frequency = Frequency + 1 where Name =%s",(nm,))
         elif 'Language' in req2 or 'language' in req2 or 'Translator' in req2 or 'translator' in req2 or 'Translate' in req2 or 'translate' in req2:
-            from subprocess import call
-            call(["python", "langtranslate.py"])
+            # from subprocess import call
+            # call(["python", "langtranslate.py"])
+            from langtranslate import speech_translate
+            speech_translate(cursor)
             time.sleep(5)
             engine.runAndWait()
             cursor.execute("UPDATE chathistory SET Frequency = Frequency + 1 where Name =%s",(nm,))
         elif 'YouTube' in req2 or 'video download' in req2 or 'downloader' in req2 or 'Downloader' in req2: 
-            engine.say(ytd())
+            from ytdownloader import ytfunc
+            print("Cia: ")
+            engine.say(ytfunc(cursor))
             engine.runAndWait()            
             cursor.execute("UPDATE chathistory SET Frequency = Frequency + 1 where Name =%s",(nm,))
         else:
@@ -219,5 +242,4 @@ if __name__ == "__main__":
         audio()
 
     #cursor.execute("ALTER table scoreboard ORDER BY Total_wins DESC")
-    db.commit()
-
+    #db.commit()
