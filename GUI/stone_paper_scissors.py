94bd32cbb1a581024c7a93165c6b6e089da39687
rohitbari2000 <60724791+rohitbari2000@users.noreply.github.com>
2021-02-03 13:20:03 +0530

    Update stone_paper_scissors.py

--- GUI/stone_paper_scissors.py
+++ GUI/stone_paper_scissors.py
@@ -17,30 +17,31 @@ player = ''
 rounds = 0
 user_choice = 0
 def spsm(cursor):
-	def accept():
-		global player
-		player = name_entry.get("1.0", 'end-1c').strip()
-		name_entry.delete("0.0", END)
+	# def accept():
+	# 	global player
+	# 	player = name_entry.get("1.0", 'end-1c').strip()
+	# 	name_entry.delete("0.0", END)
 
-		if player != '':
-			ChatLog.config(state=NORMAL)
-			ChatLog.insert(END, current_time+' ', ("small", "right", "greycolour"))
-			ChatLog.window_create(END, window=Label(ChatLog, fg="#000000", text=player, 
-			wraplength=200, font=("Arial", 10, "bold"), bg="skyblue", bd=4, justify="left"))
-			ChatLog.insert(END,'\n ', "left")
-			ChatLog.config(foreground="#0000CC", font=("Helvetica", 9))
-			ChatLog.yview(END)
+	# 	if player != '':
+	# 		ChatLog.config(state=NORMAL)
+	# 		ChatLog.insert(END, current_time+' ', ("small", "right", "greycolour"))
+	# 		ChatLog.window_create(END, window=Label(ChatLog, fg="#000000", text=player, 
+	# 		wraplength=200, font=("Arial", 10, "bold"), bg="skyblue", bd=4, justify="left"))
+	# 		ChatLog.insert(END,'\n ', "left")
+	# 		ChatLog.config(foreground="#0000CC", font=("Helvetica", 9))
+	# 		ChatLog.yview(END)
 
 	#to store the choice input
-	def choice_fun(msg): 
-		global user_choice
-		user_choice = msg
+
+	# def choice_fun(msg): 
+	# 	global user_choice
+	# 	user_choice = msg
 
 	def send():
 		msg = EntryBox.get("1.0", 'end-1c').strip()
 		EntryBox.delete("0.0", END)
 
-		if msg != '' and ('1' in msg or '2' in msg or '3' in msg):
+		if msg != '':
 			ChatLog.config(state=NORMAL)
 			ChatLog.insert(END, current_time+' ', ("small", "right", "greycolour"))
 			ChatLog.window_create(END, window=Label(ChatLog, fg="#000000", text=msg, 
@@ -48,15 +49,15 @@ def spsm(cursor):
 			ChatLog.insert(END,'\n ', "left")
 			ChatLog.config(foreground="#0000CC", font=("Helvetica", 9))
 			ChatLog.yview(END)
-			choice_fun(msg) 
-		else:
-			ChatLog.config(state=NORMAL)
-			ChatLog.insert(END, current_time+' ', ("small", "right", "greycolour"))
-			ChatLog.window_create(END, window=Label(ChatLog, fg="#000000", text=msg, 
-			wraplength=200, font=("Arial", 10, "bold"), bg="skyblue", bd=4, justify="left"))
-			ChatLog.insert(END,'\n ', "left")
-			ChatLog.config(foreground="#0000CC", font=("Helvetica", 9))
-			ChatLog.yview(END)
+			# choice_fun(msg) 
+		# else:
+		# 	ChatLog.config(state=NORMAL)
+		# 	ChatLog.insert(END, current_time+' ', ("small", "right", "greycolour"))
+		# 	ChatLog.window_create(END, window=Label(ChatLog, fg="#000000", text=msg, 
+		# 	wraplength=200, font=("Arial", 10, "bold"), bg="skyblue", bd=4, justify="left"))
+		# 	ChatLog.insert(END,'\n ', "left")
+		# 	ChatLog.config(foreground="#0000CC", font=("Helvetica", 9))
+		# 	ChatLog.yview(END)
 
 	#function to receive the response from CIA and print it on screen
 	def receive(response):        
@@ -75,9 +76,9 @@ def spsm(cursor):
 	top.geometry("400x500")
 	top.resizable(width=FALSE, height=FALSE)
 	ChatLog = Text(top, bd=0, bg="white", height="8", width="50", font="Arial",)
-	nameButton = Button(top, font=("Verdana",12,'bold'), text="Accept", width="12", height=5,
-						bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-						command= accept)
+	# nameButton = Button(top, font=("Verdana",12,'bold'), text="Accept", width="12", height=5,
+	# 					bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+	# 					command= accept)
 
 	#player = simpledialog.askstring("Input", "What is your first name?",parent=top)
 
@@ -96,7 +97,7 @@ def spsm(cursor):
 	#                     command= accept)
 
 	#Create the box to enter message
-	name_entry = Text(top, bd=0, bg="white",width="29", height="5", font="Arial")
+	#name_entry = Text(top, bd=0, bg="white",width="29", height="5", font="Arial")
 	EntryBox = Text(top, bd=0, bg="white",width="29", height="5", font="Arial")
 	#EntryBox.bind("<Return>", send)
 
@@ -104,13 +105,13 @@ def spsm(cursor):
 	#Place all components on the screen
 	scrollbar.place(x=376,y=6, height=386)
 	ChatLog.place(x=6,y=62, height=380, width=370)
-	name_entry.place(x=6,y=6, height=50, width=250)
+	#name_entry.place(x=6,y=6, height=50, width=250)
 	EntryBox.place(x=6, y=451, height=50, width=250)
 	SendButton.place(x=262, y=451, height=50, width = 70)
-	nameButton.place(x=262, y=6, height=50, width = 70)
-	n = simpledialog.askstring("Input", "How many rounds would you like to play?",parent=top) #no. of rounds
-	global rounds
-	rounds = int(n)
+	#nameButton.place(x=262, y=6, height=50, width = 70)
+	global player
+	player = simpledialog.askstring("Input", "Enter the name of player:",parent=top)
+	#print(type(rounds))
 	name_msg = "Enter the name of player:"
 	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(name_msg,player))
 	cursor.execute("INSERT INTO chathistory (Name) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(player,player))
@@ -120,26 +121,32 @@ def spsm(cursor):
 	# 								3) Scissor vs Paper => Scissor wins\n"""
 	receive(sps_db)
 	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ON DUPLICATE KEY UPDATE Name=%s",(sps_db,player))
+
 	winner_db = "Cia won the game. Better luck next time!"
 	tie_msg = "It's a tie!"
 	user_count = 0
 	comp_count = 0
 	i = 0
 	rounds_db = "How many rounds would you like to play?"
+	n = simpledialog.askstring("Input", "How many rounds would you like to play?",parent=top) #no. of rounds
+	global rounds
+	rounds = int(n)
+
 	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ",(rounds_db,))
 	cursor.execute("INSERT INTO chathistory (User) VALUES (%s) ",(n,))
+	
 	while i < rounds:
 		choice_db = "Enter your choice: 1) Rock 2) Paper 3) Scissor"
 		receive(choice_db)
-		# user_choice = int(input(choice_db + "\n"))
+		user_choice = int(simpledialog.askstring("Input", "Your choice:",parent=top))
 		cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s) ",(choice_db,))
 		cursor.execute("INSERT INTO chathistory (User) VALUES (%s) ",(user_choice,))
-		while(user_choice > 3 or user_choice < 1):
-			valid_msg = "Enter valid choice:"
-			receive(valid_msg)
-			#user_choice = int(input(valid_msg + "\n"))
-			cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s)",(valid_msg,))
-			cursor.execute("INSERT INTO chathistory (User) VALUES (%s)",(user_choice,))
+		# while(user_choice > 3 or user_choice < 1):
+		# 	valid_msg = "Enter valid choice:"
+		# 	receive(valid_msg)
+		# 	#user_choice = int(input(valid_msg + "\n"))
+		# 	cursor.execute("INSERT INTO chathistory (Cia) VALUES (%s)",(valid_msg,))
+		# 	cursor.execute("INSERT INTO chathistory (User) VALUES (%s)",(user_choice,))
 
 		if user_choice == 1:
 			user_choice_name = 'Rock'
@@ -219,13 +226,13 @@ def spsm(cursor):
 		cursor.execute("UPDATE scoreboard SET Stone_Paper_Scissors = Stone_Paper_Scissors+1 WHERE Name=%s",(player,))
 		cursor.execute("UPDATE scoreboard SET Total_wins = Total_wins+1 WHERE Name=%s",(player,))
 		#print ("\nHurray!!" +player+ " won!!")
-		last_game_msg = "\nHurray!!" +player+ " won!!"
+		last_game_msg = "Hurray!!" +player+ " won!!"
 		receive(last_game_msg)
 		cursor.execute("INSERT INTO chathistory (Cia) VALUES ('Hurray')")
 		cursor.execute("INSERT INTO chathistory (Cia) VALUES ('You')")
 		cursor.execute("INSERT INTO chathistory (Cia) VALUES ('won')")
 
-	score_msg = "\nScore:\nCia: " + str(comp_count) + "\n" + "You" + ":" + str(user_count)
+	score_msg = "Score: Cia: " + str(comp_count) + " You" + ":" + str(user_count)
 	#print("\nScore:\nCia: " + str(comp_count) + "\n" + "You" + ":" + str(user_count))
 	receive(score_msg)
 	final_msg = "Thanks for playing! I hope you had fun!"
